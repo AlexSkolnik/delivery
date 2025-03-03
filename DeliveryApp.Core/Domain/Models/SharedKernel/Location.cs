a4328ee287dc30f0a13ed32550d0d106a8ae4481
AlekseySkolnik <100571235+AlekseySkolnik@users.noreply.github.com>
2025-03-03 18:27:28 +0300

    add locaation

---
+++ DeliveryApp.Core/Domain/Models/SharedKernel/Location.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using CSharpFunctionalExtensions;
+using Primitives;
+
+namespace DeliveryApp.Core.Domain.Models.SharedKernel;
+
+public class Location : ValueObject
+{
+    /// <summary>
+    /// Ctr
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    private Location()
+    {
+    }
+
+    /// <summary>
+    /// Ctor
+    /// </summary>
+    /// <param name="x">X coordinate</param>
+    /// <param name="y">Y coordinate</param>
+    private Location(int x, int y) : this()
+    {
+        X = x;
+        Y = y;
+    }
+
+    public int X { get; }
+    public int Y { get; }
+
+    public static Result<Location, Error> Create(int x, int y)
+    {
+        if (x is < 1 or > 10) return GeneralErrors.ValueIsInvalid(nameof(x));
+        if (y is < 1 or > 10) return GeneralErrors.ValueIsInvalid(nameof(y));
+
+        return new Location(x, y);
+    }
+
+    public int GetDistanceToLocation(Location start)
+    {
+        if (start == this)
+            return 0;
+
+        return Convert.ToInt32(Math.Abs(start.X - X) + Math.Abs(start.Y - Y));
+    }
+
+    public static Location CreateRandomLocation()
+    {
+        var random = new Random();
+
+        random.Next(1, 100);
+
+        return new Location(random.Next(1, 10), random.Next(1, 10));
+    }
+
+    /// <summary>
+    /// Перегрузка для определения идентичности
+    /// </summary>
+    /// <returns>Результат</returns>
+    /// <remarks>Идентичность будет происходить по совокупности полей указанных в методе</remarks>
+    [ExcludeFromCodeCoverage]
+    protected override IEnumerable<IComparable> GetEqualityComponents()
+    {
+        yield return X;
+        yield return Y;
+    }
+}
---
+++ Tests/DeliveryApp.UnitTests/Domain/Models/SharedKernel/LocationShould.cs
@@ -0,0 +1,83 @@
+﻿using Xunit;
+using DeliveryApp.Core.Domain.Models.SharedKernel;
+using FluentAssertions;
+
+namespace DeliveryApp.UnitTests.Domain.Models.SharedKernel;
+
+public class LocationShould
+{
+    [Fact]
+    public void BeCorrectWhenParamsIsCorrectOnCreated()
+    {
+        //Arrange
+
+        //Act
+        var location = Location.Create(1, 2);
+
+        //Assert
+        location.IsSuccess.Should().BeTrue();
+        location.Value.X.Should().Be(1);
+        location.Value.Y.Should().Be(2);
+    }
+
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(0, 1)]
+    [InlineData(1, 0)]
+    [InlineData(1, 11)]
+    [InlineData(11, 1)]
+    [InlineData(11, 11)]
+    public void ReturnErrorWhenParamsIsInCorrectOnCreated(int x, int y)
+    {
+        //Arrange
+
+        //Act
+        var weight = Location.Create(x, y);
+
+        //Assert
+        weight.IsSuccess.Should().BeFalse();
+        weight.Error.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void BeEqualWhenAllPropertiesIsEqual()
+    {
+        //Arrange
+        var first = Location.Create(1, 1).Value;
+        var second = Location.Create(1, 1).Value;
+
+        //Act
+        var result = first == second;
+
+        //Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void BeNotEqualWhenNotAllPropertiesIsEqual()
+    {
+        //Arrange
+        var first = Location.Create(1, 1).Value;
+        var second = Location.Create(1, 2).Value;
+
+        //Act
+        var result = first == second;
+
+        //Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void GetDistanceToLocation()
+    {
+        //Arrange
+        var first = Location.Create(10, 10).Value;
+        var second = Location.Create(1, 1).Value;
+
+        //Act
+        var distance = first.GetDistanceToLocation(second);
+
+        //Assert
+        distance.Should().Be(18);
+    }
+}
