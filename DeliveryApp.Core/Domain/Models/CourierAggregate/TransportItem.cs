bccb060de83db7cbeff6b911eee531c911409a78
AlekseySkolnik <100571235+AlekseySkolnik@users.noreply.github.com>
2025-03-10 08:43:01 +0300

    save

---
+++ DeliveryApp.Core/Domain/Models/CourierAggregate/TransportItem.cs
@@ -0,0 +1,81 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using CSharpFunctionalExtensions;
+using Primitives;
+
+namespace DeliveryApp.Core.Domain.Models.CourierAggregate;
+
+public class TransportItem : Entity<int>
+{
+    public static readonly TransportItem Pedestrian = new(1, nameof(Pedestrian).ToLowerInvariant(), 1);
+    public static readonly TransportItem Bicycle = new(2, nameof(Bicycle).ToLowerInvariant(), 2);
+    public static readonly TransportItem Car = new(3, nameof(Car).ToLowerInvariant(), 3);
+
+    [ExcludeFromCodeCoverage]
+    private TransportItem()
+    {
+    }
+
+    private TransportItem(int id, string name, int speed) : this()
+    {
+        Id = id;
+        Name = name;
+        Speed = speed;
+    }
+
+    public string Name { get; private set; }
+    public int Speed { get; private set; }
+
+    /// <summary>
+    /// Список всех значений списка
+    /// </summary>
+    public static IEnumerable<TransportItem> GetItems()
+    {
+        yield return Pedestrian;
+        yield return Bicycle;
+        yield return Car;
+    }
+
+    /// <summary>
+    /// Получить транспорт по названию
+    /// </summary>
+    public static Result<TransportItem, Error> GetByName(string name)
+    {
+        var items = GetItems()
+            .SingleOrDefault(s => string.Equals(s.Name, name, StringComparison.CurrentCultureIgnoreCase));
+        
+        if (items == null)
+        {
+            return Errors.StatusIsWrong();
+        }
+
+        return items;
+    }
+
+    /// <summary>
+    /// Получить транспорт по идентификатору
+    /// </summary>
+    public static Result<TransportItem, Error> GetById(int id)
+    {
+        var state = GetItems().SingleOrDefault(s => s.Id == id);
+        
+        if (state == null)
+        {
+            return Errors.StatusIsWrong();
+        }
+
+        return state;
+    }
+
+    /// <summary>
+    /// Ошибки, которые может возвращать сущность
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public static class Errors
+    {
+        public static Error StatusIsWrong()
+        {
+            return new Error($"{nameof(TransportItem).ToLowerInvariant()}_status_is_wrong",
+                $"Не верное значение. Допустимые значения: {nameof(TransportItem).ToLowerInvariant()}: {string.Join(",", GetItems().Select(s => s.Name))}");
+        }
+    }
+}
---
+++ Tests/DeliveryApp.UnitTests/Domain/Models/CourierAggregate/TransportItemShould.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections.Generic;
+using CSharpFunctionalExtensions;
+using DeliveryApp.Core.Domain.Models.CourierAggregate;
+using FluentAssertions;
+using Xunit;
+
+namespace DeliveryApp.UnitTests.Domain.Models.CourierAggregate;
+
+public sealed class TransportItemShould : Entity<int>
+{
+    public static IEnumerable<object[]> GetTransports()
+    {
+        yield return [TransportItem.Pedestrian, 1, "pedestrian", 1];
+        yield return [TransportItem.Bicycle, 2, "bicycle", 2];
+        yield return [TransportItem.Car, 3, "car", 3];
+    }
+
+    [Theory]
+    [MemberData(nameof(GetTransports))]
+    public void ReturnCorrectIdAndName(TransportItem transportItem, int id, string name, int speed)
+    {
+        //Arrange
+
+        //Act
+
+        //Assert
+        transportItem.Id.Should().Be(id);
+        transportItem.Name.Should().Be(name);
+        transportItem.Speed.Should().Be(speed);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetTransports))]
+    public void CanBeFoundById(TransportItem transportItem, int id, string name, int speed)
+    {
+        //Arrange
+
+        //Act
+        var result = TransportItem.GetById(id).Value;
+
+        //Assert
+        result.Should().Be(transportItem);
+        result.Id.Should().Be(id);
+        result.Name.Should().Be(name);
+        result.Speed.Should().Be(speed);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetTransports))]
+    public void CanBeFoundByName(TransportItem transportItem, int id, string name, int speed)
+    {
+        //Arrange
+
+        //Act
+        var result = TransportItem.GetByName(name).Value;
+
+        //Assert
+        result.Should().Be(transportItem);
+        result.Id.Should().Be(id);
+        result.Name.Should().Be(name);
+        result.Speed.Should().Be(speed);
+    }
+
+    [Fact]
+    public void ReturnErrorWhenTransportNotFoundById()
+    {
+        //Arrange
+        var id = -1;
+
+        //Act
+        var result = TransportItem.GetById(id);
+
+        //Assert
+        result.IsSuccess.Should().BeFalse();
+        result.Error.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void ReturnErrorWhenTransportNotFoundByName()
+    {
+        //Arrange
+        var name = "not-existed-TransportItem";
+
+        //Act
+        var result = TransportItem.GetByName(name);
+
+        //Assert
+        result.IsSuccess.Should().BeFalse();
+        result.Error.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void ReturnListOfStatuses()
+    {
+        //Arrange
+
+        //Act
+        var allStatuses = TransportItem.GetItems();
+
+        //Assert
+        allStatuses.Should().NotBeEmpty();
+    }
+
+    [Fact]
+    public void DerivedEntity()
+    {
+        //Arrange
+
+        //Act
+        var isDerivedEntity = typeof(TransportItem).IsSubclassOf(typeof(Entity<int>));
+
+        //Assert
+        isDerivedEntity.Should().BeTrue();
+    }
+
+    [Fact]
+    public void BeEqualWhenIdIsEqual()
+    {
+        //Arrange
+        var pedestrian1 = TransportItem.Pedestrian;
+        var pedestrian2 = TransportItem.Pedestrian;
+        pedestrian1.Id.Should().Be(pedestrian2.Id);
+
+        //Act
+        var result = pedestrian1.Equals(pedestrian2);
+
+        //Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void NotBeEqualWhenIdIsNotEqual()
+    {
+        //Arrange
+        var pedestrian = TransportItem.Pedestrian;
+        var car = TransportItem.Car;
+        pedestrian.Id.Should().NotBe(car.Id);
+
+        //Act
+        var result = pedestrian.Equals(car);
+
+        //Assert
+        result.Should().BeFalse();
+    }
+}
