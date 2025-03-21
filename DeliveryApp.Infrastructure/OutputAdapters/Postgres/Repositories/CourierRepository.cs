bcf8e5c2bed99323bad726d7104fcbc2bb765efe
AlekseySkolnik <100571235+AlekseySkolnik@users.noreply.github.com>
2025-03-21 08:54:09 +0300

    add infrastructure and tests

---
+++ DeliveryApp.Infrastructure/OutputAdapters/Postgres/Repositories/CourierRepository.cs
@@ -0,0 +1,30 @@
+﻿using CSharpFunctionalExtensions;
+using DeliveryApp.Core.Domain.Models.CourierAggregate;
+using DeliveryApp.Core.Ports;
+
+namespace DeliveryApp.Infrastructure.OutputAdapters.Postgres.Repositories;
+
+public class CourierRepository(ApplicationDbContext context) : ICourierRepository
+{
+    private readonly ApplicationDbContext _context = context;
+
+    public Task AddAsync(Courier courier)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Courier courier)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Maybe<Courier>> GetAsync(Guid courierId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Courier> GetAllFree()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ DeliveryApp.Infrastructure/OutputAdapters/Postgres/Repositories/OrderRepository.cs
@@ -0,0 +1,35 @@
+﻿using CSharpFunctionalExtensions;
+using DeliveryApp.Core.Domain.Models.OrderAggregate;
+using DeliveryApp.Core.Ports;
+
+namespace DeliveryApp.Infrastructure.OutputAdapters.Postgres.Repositories;
+
+public class OrderRepository(ApplicationDbContext context) : IOrderRepository
+{
+    private readonly ApplicationDbContext _context = context;
+
+    public Task AddAsync(Order order)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Order order)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Maybe<Order>> GetAsync(Guid orderId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Maybe<Order>> GetFirstInCreatedStatus()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Order> GetAllAssigned()
+    {
+        throw new NotImplementedException();
+    }
+}
